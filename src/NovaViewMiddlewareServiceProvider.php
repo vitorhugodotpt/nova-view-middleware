68137a67f8c7d69caaa19772913a9f4f4b048e5c
Vitor Faria <vfaria@pdmfc.com>
2020-10-17 00:23:53 +0100

    Clear project

--- src/NovaViewMiddlewareServiceProvider.php
+++ src/NovaViewMiddlewareServiceProvider.php
@@ -11,37 +11,7 @@ class NovaViewMiddlewareServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'nova-view-middleware');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'nova-view-middleware');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('nova-view-middleware.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/nova-view-middleware'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/nova-view-middleware'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/nova-view-middleware'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
-        }
     }
 
     /**
@@ -49,9 +19,6 @@ class NovaViewMiddlewareServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'nova-view-middleware');
-
         // Register the main class to use with the facade
         $this->app->singleton('nova-view-middleware', function () {
             return new NovaViewMiddleware;
